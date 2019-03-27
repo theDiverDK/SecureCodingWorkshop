e3a6e68fffcb4a32ac75fb435b94d8c4f52b2f0a
Søren Reinke <soren@reinke.dk>
2019-03-27 11:39:40 +0100

    Hash and salt

--- Workshop/SecureCodingWorkshop/HashPassword/Program.cs
+++ Workshop/SecureCodingWorkshop/HashPassword/Program.cs
@@ -9,6 +9,18 @@ namespace SecureCodingWorkshop.HashPassword
         {
             // TODO : Create a hashed password that is combine with a salt.
             // TODO : Base64 encode the result and display to the console.
+
+            var password = "P@ssw0rd"; //Very secure :-)
+
+            var salt1 = Hash.GenerateSalt();
+            var salt2 = Hash.GenerateSalt();
+
+            var hashAndSalt1 = Hash.HashPasswordWithSalt(Encoding.UTF8.GetBytes(password), salt1);
+            var hashAndSalt2 = Hash.HashPasswordWithSalt(Encoding.UTF8.GetBytes(password), salt2);
+
+            Console.WriteLine($"The password: '{password}' hash and salted with salt 1 = {Convert.ToBase64String(hashAndSalt1)}");
+            Console.WriteLine($"The password: '{password}' hash and salted with salt 2 = {Convert.ToBase64String(hashAndSalt2)}");
+
         }
     }
 }
