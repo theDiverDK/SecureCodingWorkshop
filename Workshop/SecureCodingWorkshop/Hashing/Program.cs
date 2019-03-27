6aa414e4d41501792b5a47f7b8ddd65b9ef83aac
Søren Reinke <soren@reinke.dk>
2019-03-27 10:31:09 +0100

    * Hashing.cs: Hashing of text and file.
    * Program.cs, Program.cs:

--- Workshop/SecureCodingWorkshop/Hashing/Program.cs
+++ Workshop/SecureCodingWorkshop/Hashing/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Text;
 
 namespace SecureCodingWorkshop.Hashing
@@ -9,6 +10,23 @@ namespace SecureCodingWorkshop.Hashing
         {
             // TODO : Calculate MD5, SHA1, SHA256 and SHA512 hashes for a series of strings
             // TODO : Base64 encode the results and display to the console.
+            var input = "Hello, World!";
+            var inputAsByteArray = Encoding.UTF8.GetBytes(input);
+            Console.WriteLine($"Hashes of {input}");
+            CalculateAndDisplayHashes(inputAsByteArray);
+
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("Hashes of dll file");
+            var fileData = File.ReadAllBytes("Hashing.dll");
+            CalculateAndDisplayHashes(fileData);
+        }
+
+        private static void CalculateAndDisplayHashes(byte[] inputAsByteArray)
+        {
+            Console.WriteLine("MD5 = " + Convert.ToBase64String(HashData.ComputeHashMd5(inputAsByteArray)));
+            Console.WriteLine("SHA1 = " + Convert.ToBase64String(HashData.ComputeHashSha1(inputAsByteArray)));
+            Console.WriteLine("SHA256 = " + Convert.ToBase64String(HashData.ComputeHashSha256(inputAsByteArray)));
+            Console.WriteLine("SHA512 = " + Convert.ToBase64String(HashData.ComputeHashSha512(inputAsByteArray)));
         }
     }
 }
--- Workshop/SecureCodingWorkshop/RandomNumber/Program.cs
+++ Workshop/SecureCodingWorkshop/RandomNumber/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace SecureCodingWorkshop.RandomNumber
+﻿using System;
+using System.Buffers.Text;
+
+namespace SecureCodingWorkshop.RandomNumber
 {
     static class Program
     {
@@ -6,6 +9,16 @@
         {
             // TODO : Generate 10 random numbers using RNGCryptoServiceProvider.            
             // TODO : Convert the resulting byte arrays into base64 strings and print to the console.
+
+
+            for (var i = 0; i < 10; i++)
+            {
+                var randomNumber = Random.GenerateRandomNumber(32);
+
+                var result = Convert.ToBase64String(randomNumber);
+
+                Console.WriteLine($"Random number {i}: {result}");
+            }
         }
     }
 }
