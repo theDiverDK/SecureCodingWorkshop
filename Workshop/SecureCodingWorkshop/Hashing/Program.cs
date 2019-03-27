fe927e575475188e620a236ee7e5aff14e54f9f7
Søren Reinke <soren@reinke.dk>
2019-03-27 10:49:17 +0100

    * HashData.cs: Renamed file, and cleanup
    * Program.cs:

--- Workshop/SecureCodingWorkshop/Hashing/Program.cs
+++ Workshop/SecureCodingWorkshop/Hashing/Program.cs
@@ -10,12 +10,14 @@ namespace SecureCodingWorkshop.Hashing
         {
             // TODO : Calculate MD5, SHA1, SHA256 and SHA512 hashes for a series of strings
             // TODO : Base64 encode the results and display to the console.
+
             var input = "Hello, World!";
             var inputAsByteArray = Encoding.UTF8.GetBytes(input);
             Console.WriteLine($"Hashes of {input}");
             CalculateAndDisplayHashes(inputAsByteArray);
 
             Console.WriteLine(Environment.NewLine);
+
             Console.WriteLine("Hashes of dll file");
             var fileData = File.ReadAllBytes("Hashing.dll");
             CalculateAndDisplayHashes(fileData);
