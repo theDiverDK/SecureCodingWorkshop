6c5f7d1aa1a8343e21f12b936f0b380ab757b3b6
Søren Reinke <soren@reinke.dk>
2019-03-27 15:04:31 +0100

    Digital Signature and check for tampering

--- Workshop/SecureCodingWorkshop/DigitalSignature/Program.cs
+++ Workshop/SecureCodingWorkshop/DigitalSignature/Program.cs
@@ -14,9 +14,37 @@ namespace SecureCodingWorkshop.DigitalSignature
             // TODO : Hash that document with sha256
             // TODO : Create a new RSA key pair
             // TODO : Create a digital signature of the hash
-            
+
             // TODO : Check that you can verify the signature correctly.
             // TODO : Tamper with the document and then try to re-verify the signature.
+
+            var input = "Hello, World!";
+
+            var hash = ComputeHashSha256(Encoding.UTF8.GetBytes(input));
+
+            var rsa = new DigitalSignature();
+            rsa.AssignNewKey();
+
+            var digitalSignatur = rsa.SignData(hash);
+
+            var isValid = rsa.VerifySignature(hash, digitalSignatur);
+            Console.WriteLine("Untampered");
+            Console.WriteLine($"The Signature is valid {isValid}");
+            Console.WriteLine();
+            Console.WriteLine("Tampered");
+
+            input = $"{input}."; //Added . to input
+            var tamperedHash = ComputeHashSha256(Encoding.UTF8.GetBytes(input));
+            isValid = rsa.VerifySignature(tamperedHash, digitalSignatur);
+            Console.WriteLine($"The Signature is valid {isValid}");
+        }
+
+        public static byte[] ComputeHashSha256(byte[] toBeHashed)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                return sha256.ComputeHash(toBeHashed);
+            }
         }
     }
 }
